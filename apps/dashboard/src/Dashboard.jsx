6dd1f96e79da181c22fd66d956faa25385f6c536
nouar salah eddine <nouarsalheddine@gmail.com>
2023-09-04 13:01:33 +0100

    add use context

---
+++ apps/dashboard/src/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+const Dashboard = ({ dispatch }) => (
+  <div className="dashboard">
+    <p>Remote Dashboard</p>
+    <button
+      onClick={() => {
+        dispatch({
+          type: "increment",
+        });
+      }}
+    >
+      Increment
+    </button>
+  </div>
+);
